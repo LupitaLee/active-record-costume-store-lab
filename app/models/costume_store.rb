604b1d99aa32ac501d868e7a07e70c8103461a69
Lupita <lupitarivera8899@gmail.com>
2021-03-11 08:52:37 -0600

    commit tree 622e649dcb60cb7c28a8c4bc36838d63af242b7d

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,13 @@
 # Create your CostumeStore class here
+
+class CostumeStore < ActiveRecord::Base
+#class all methods
+#class first method
+#class find/ find_by
+#save instance 
+#create instance (new/save)
+#getters and setters 
+#update  ex-  Movie.first.update(title: "Pirates")
+
+
+end
